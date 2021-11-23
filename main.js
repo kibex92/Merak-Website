a724865246b1a1d6270e4421c7bf02c4e1bfdf45
Boris Knezevic <boris.knezevic0@gmail.com>
2021-11-23 18:00:38 +0100

    Add modal gallery image slider

--- main.js
+++ main.js
@@ -12,3 +12,73 @@ window.onscroll = () => {
   }
   prevScrollpos = currentScrollPos;
 };
+
+// Gallery Slider
+
+let galleryImages = document.querySelectorAll(".img-grid");
+let getLatestOpenedImg;
+let windowWith = window.innerWidth;
+
+galleryImages.forEach((img, index) => {
+  img.onclick = () => {
+    getLatestOpenedImg = index + 1;
+
+    let container = document.body;
+    let newImgWindow = document.createElement("div");
+    container.appendChild(newImgWindow);
+    newImgWindow.classList.add("img-window");
+    newImgWindow.setAttribute("onclick", "closeImg()");
+
+    let newImg = img.cloneNode(true);
+    newImgWindow.appendChild(newImg);
+    newImg.classList.remove("img-grid");
+    newImg.classList.add("popup-img");
+    newImg.setAttribute("id", "current-img");
+
+    newImg.onload = () => {
+      let newNextBtn = document.createElement("a");
+      newNextBtn.innerHTML = '<i class="fas fa-chevron-right"></i>';
+      container.appendChild(newNextBtn);
+      newNextBtn.classList.add("img-btn-next");
+      newNextBtn.setAttribute("onclick", "nextImg(1)");
+
+      let newPrevBtn = document.createElement("a");
+      newPrevBtn.innerHTML = '<i class="fas fa-chevron-left"></i>';
+      container.appendChild(newPrevBtn);
+      newPrevBtn.classList.add("img-btn-prev");
+      newPrevBtn.setAttribute("onclick", "nextImg(0)");
+    };
+  };
+});
+
+closeImg = () => {
+  document.querySelector(".img-window").remove();
+  document.querySelector(".img-btn-next").remove();
+  document.querySelector(".img-btn-prev").remove();
+};
+
+nextImg = (next) => {
+  document.querySelector("#current-img").remove();
+
+  let getImgWindow = document.querySelector(".img-window");
+  let newImg = document.createElement("img");
+  getImgWindow.appendChild(newImg);
+
+  let calcNewImg;
+  if (next === 1) {
+    calcNewImg = getLatestOpenedImg + 1;
+    if (calcNewImg > galleryImages.length) {
+      calcNewImg = 1;
+    }
+  } else if (next === 0) {
+    calcNewImg = getLatestOpenedImg - 1;
+    if (calcNewImg < 1) {
+      calcNewImg = galleryImages.length;
+    }
+  }
+  newImg.setAttribute("src", `./images/gallery/img-${calcNewImg}.jpg`);
+  newImg.classList.add("popup-img");
+  newImg.setAttribute("id", "current-img");
+
+  getLatestOpenedImg = calcNewImg;
+};
