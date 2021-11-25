a18b64a41fc5b0208a2140e2d7e3dca4101fd560
Boris Knezevic <boris.knezevic0@gmail.com>
2021-11-25 22:21:23 +0100

    css stuff in progress

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".hamburger").style.top = "0";
-    document.querySelector("#back-arrow").style.top = "20px";
   } else {
     document.querySelector(".hamburger").style.top = "-100px";
-    document.querySelector("#back-arrow").style.top = "-100px";
+    // document.querySelector("#back-arrow").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
 };
