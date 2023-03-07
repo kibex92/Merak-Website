a24a42c1f33e0d865541828eeeeb6f355633a7a1
Boris Knezevic <boris.knezevic0@gmail.com>
2023-03-07 11:58:26 +0100

    add dynamic caching

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-const staticCacheName = "site-static"
+const staticCacheName = "site-static-v1"
+const dynamicCache = "site-dynamic-v1"
 const assets = [
     "/",
     "/index.html",
     "/main.js",
     "/css/style.css",
+    "/css/animations.css",
+    "/css/gallery.css",
+    "/css/components/banner.css",
+    "/css/components/button.css",
+    "/css/components/card.css",
+    "/css/components/footer.css",
+    "/css/components/form.css",
+    "/css/components/menu.css",
     "/images/header-FINAL.png",
     "/images/merak-website-header.png",
     "/images/symbol.png",
     "images/gallery/Boris.png",
-    // "/images/gallery/Cris.png",
-    // "/images/gallery/Moritz.png",
-    // "/images/gallery/img-1.png",
-    // "/images/gallery/img-2.png",
-    // "/images/gallery/img-3.png",
-    // "/images/gallery/img-4.png",
-    // "/images/gallery/img-5.png",
-    // "/images/gallery/img-6.png",
-    // "/images/gallery/img-7.png",
-    // "/images/gallery/img-8.png",
     "https://use.fontawesome.com/releases/v5.15.4/css/all.css",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
-    "https://s.pageclip.co/v1/pageclip.css"
+    "https://s.pageclip.co/v1/pageclip.css",
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
 ]
 
 self.addEventListener("install", event => {
@@ -38,6 +38,13 @@ self.addEventListener("install", event => {
 // activate event
 self.addEventListener("activate", event => {
     // console.log("service worker has been activated");
+    event.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys.filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key)))
+        })
+    )
 })
 
 // fetch event
@@ -45,7 +52,12 @@ self.addEventListener("fetch", event => {
     // console.log("fetch event", event);
     event.respondWith(
         caches.match(event.request).then(cacheResponse => {
-            return cacheRes || fetch(event.request)
+            return cacheResponse || fetch(event.request).then(fetchResponse => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(event.request.url, fetchResponse.clone());
+                    return fetchResponse;
+                })
+            })
         })
     )
 })
