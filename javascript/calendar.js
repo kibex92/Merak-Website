c74dc6c3d13a4c71c82a424f6524ae2c047fe4e2
Boris Knezevic <boris.knezevic0@gmail.com>
2023-08-22 17:31:16 +0200

    reorganised JS

---
+++ javascript/calendar.js
@@ -0,0 +1,44 @@
+// Dynamic Calendar
+
+let today = new Date();
+let eventContainers = document
+	.querySelector(".events")
+	.getElementsByClassName("event-container");
+
+for (let event of eventContainers) {
+	concerts.forEach((concert) => {
+		let eventDate = new Date(concert.year, concert.month - 1, concert.day);
+
+		if (event.id !== concert.year.toString()) {
+			return;
+		}
+
+		let position =
+			today < eventDate ||
+			today.setHours(0, 0, 0, 0) == eventDate.setHours(0, 0, 0, 0)
+				? "afterbegin"
+				: "beforeend";
+		let pastClass =
+			today < eventDate ||
+			today.setHours(0, 0, 0, 0) == eventDate.setHours(0, 0, 0, 0)
+				? ""
+				: "past";
+
+		let eventDiv = `<div class="event-info ${pastClass}">
+									<div class="date">
+										<p id="day">${concert.day}.</p>
+										<p id="month">${concert.month}.</p>
+									</div>
+									<div class="venue-container">
+										<div class="venues">
+											<p class="city ${pastClass}">${concert.city}</p>
+											<p class="hall">${concert.hall}</p>
+										</div>
+									</div>
+									<a href="${concert.link}" style="display: inline" target="_blank"
+										class="btn-gold ${concert.visibility}">${concert.linkText}</a>
+								</div>`;
+
+		event.insertAdjacentHTML(position, eventDiv);
+	});
+}
---
+++ javascript/gallery.js
@@ -0,0 +1,91 @@
+// Gallery Slider
+let galleryImages = document.querySelectorAll(".img-grid");
+let getLatestOpenedImg;
+let windowWith = window.innerWidth;
+
+galleryImages.forEach((img, index) => {
+	img.onclick = () => {
+		toggleBlur();
+		getLatestOpenedImg = index + 1;
+		let container = document.body;
+		let newImgWindow = document.createElement("div");
+		container.appendChild(newImgWindow);
+		newImgWindow.classList.add("img-window");
+		newImgWindow.setAttribute("onclick", "closeImg()");
+
+		let newImg = img.cloneNode(true);
+		newImgWindow.appendChild(newImg);
+		newImg.classList.remove("img-grid");
+		newImg.classList.add("popup-img");
+		newImg.setAttribute("id", "current-img");
+
+		newImg.onload = () => {
+			let newNextBtn = document.createElement("a");
+			newNextBtn.innerHTML = '<i class="fas fa-chevron-right"></i>';
+			container.appendChild(newNextBtn);
+			newNextBtn.classList.add("img-btn-next");
+			newNextBtn.setAttribute("onclick", "nextImg(1)");
+
+			let newPrevBtn = document.createElement("a");
+			newPrevBtn.innerHTML = '<i class="fas fa-chevron-left"></i>';
+			container.appendChild(newPrevBtn);
+			newPrevBtn.classList.add("img-btn-prev");
+			newPrevBtn.setAttribute("onclick", "nextImg(0)");
+		};
+	};
+});
+
+closeImg = () => {
+	document.querySelector(".img-window").remove();
+	document.querySelector(".img-btn-next").remove();
+	document.querySelector(".img-btn-prev").remove();
+	toggleBlur();
+};
+
+nextImg = (next) => {
+	document.querySelector("#current-img").remove();
+
+	let getImgWindow = document.querySelector(".img-window");
+	let newImg = document.createElement("img");
+	getImgWindow.appendChild(newImg);
+	let calcNewImg;
+	if (next === 1) {
+		console.log("next is", next);
+		console.log("latest opened img is", getLatestOpenedImg);
+
+		calcNewImg = getLatestOpenedImg + 1;
+		console.log("calc new img is ", calcNewImg);
+		if (calcNewImg > galleryImages.length) {
+			calcNewImg = 1;
+		}
+	} else if (next === 0) {
+		calcNewImg = getLatestOpenedImg - 1;
+
+		if (calcNewImg < 1) {
+			calcNewImg = galleryImages.length;
+		}
+	}
+	newImg.setAttribute("src", `./images/gallery/img-${calcNewImg}.jpg`);
+	newImg.classList.add("popup-img");
+	newImg.setAttribute("id", "current-img");
+
+	getLatestOpenedImg = calcNewImg;
+};
+
+// Blur
+toggleBlur = () => {
+	const blur = document.getElementById("blur");
+	blur.classList.toggle("active");
+};
+
+document.addEventListener("keydown", (e) => {
+	if (e.key == "ArrowLeft") {
+		nextImg(0);
+	}
+	if (e.key == "ArrowRight") {
+		nextImg(1);
+	}
+	if (e.key == "Escape") {
+		closeImg();
+	}
+});
