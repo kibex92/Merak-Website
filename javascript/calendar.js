904b304759d482a150112f66225a08d4767d78ea
Boris Knezevic <boris.knezevic0@gmail.com>
2023-11-12 14:02:00 +0100

    add styling for cancalled concerts

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -6,8 +6,31 @@ let eventContainers = document
 const monthTemplate = document.getElementById("month-template");
 const monthsMap = new Map();
 
+const generateConcertInfo = (concert) => {
+  const concertInfo = document.createElement('div');
+  concertInfo.classList.add('event-info');
+
+  if (concert.cancelled) {
+    concertInfo.innerHTML = `
+      <h3 class="date line-through">${concert.day}.${concert.month}</h3>
+      <h3 class="city line-through">${concert.city} </h3>
+      <p class="venue line-through">${concert.hall}</p>
+      <a href="${concert.link}" class="tickets btn-gold ${concert.visibility}" target="_blank">${concert.linkText}</a>
+    `;
+  } else {
+    concertInfo.innerHTML = `
+      <h3 class="date">${concert.day}.${concert.month}</h3>
+      <h3 class="city">${concert.city}</h3>
+      <p class="venue">${concert.hall}</p>
+      <a href="${concert.link}" class="tickets btn-gold ${concert.visibility}" target="_blank">${concert.linkText}</a>
+    `;
+  }
+
+  return concertInfo;
+}
+
 concerts.forEach((concert) => {
-	// Create unique month key to be used with the monthsMap
+  // Create unique month key to be used with the monthsMap
   const monthKey = `${concert.month}-${concert.year}`;
 
   // Check if the month-section already exists
@@ -18,16 +41,8 @@ concerts.forEach((concert) => {
     monthsMap.set(monthKey, newMonthSection);
   }
 
-	// Create a div with concert details to append to the month section
-  const concertInfo = document.createElement('div');
-  concertInfo.classList.add('event-info');
-  concertInfo.innerHTML = `
-    
-    <h3 class="date">${concert.day}.${concert.month}</h3>
-    <h3 class="city">${concert.city}</h3>
-    <p class="venue">${concert.hall}</p>
-    <a href="${concert.link}" class="tickets btn-gold ${concert.visibility}" target="_blank">${concert.linkText}</a>
-  `;
+  // Create a div with concert details to append to the month section
+  const concertInfo = generateConcertInfo(concert);
 
   // Append the concert info to the corresponding month-section
   monthsMap.get(monthKey).appendChild(concertInfo);
