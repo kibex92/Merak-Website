9d09c8b54eaa9c45e45efd422461c37851d89153
Boris Knezevic <boris.knezevic0@gmail.com>
2024-01-11 14:21:27 +0100

    add newsletter form

---
+++ javascript/newsletter.js
@@ -0,0 +1,19 @@
+window.REQUIRED_CODE_ERROR_MESSAGE = 'Wählen Sie bitte einen Ländervorwahl aus.';
+window.LOCALE = 'de';
+window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "Die eingegebenen Informationen sind nicht gültig. Bitte überprüfen Sie das Feldformat und versuchen Sie es erneut.";
+
+window.REQUIRED_ERROR_MESSAGE = "Dieses Feld darf nicht leer sein. ";
+
+window.GENERIC_INVALID_MESSAGE = "Die eingegebenen Informationen sind nicht gültig. Bitte überprüfen Sie das Feldformat und versuchen Sie es erneut.";
+
+
+
+
+window.translation = {
+    common: {
+        selectedList: '{quantity} Liste ausgewählt',
+        selectedLists: '{quantity} Listen ausgewählt'
+    }
+};
+
+var AUTOHIDE = Boolean(0);
