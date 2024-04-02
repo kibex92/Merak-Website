eddca2fbd4086fecee1d7ea903e9f80e1e911e75
Boris Knezevic <boris.knezevic0@gmail.com>
2024-04-02 12:53:33 +0200

    lazy load recaptcha

--- javascript/utility.js
+++ javascript/utility.js
@@ -67,3 +67,21 @@ menuBtn.addEventListener("click", () => {
     hideCarousel()
   }
 })
+
+// Lazy load recaptcha API on form focus
+function reCaptchaOnFocus() {
+  var head = document.getElementsByTagName('head')[0]
+  var script = document.createElement('script')
+  script.type = 'text/javascript';
+  script.src = 'https://www.google.com/recaptcha/api.js?hl=de'
+  head.appendChild(script);
+
+  // remove focus to avoid js error:
+  document.getElementById('VORNAME').removeEventListener('focus', reCaptchaOnFocus)
+  document.getElementById('NACHNAME').removeEventListener('focus', reCaptchaOnFocus)
+  document.getElementById('EMAIL').removeEventListener('focus', reCaptchaOnFocus)
+};
+// add initial event listener to the form inputs
+document.getElementById('VORNAME').addEventListener('focus', reCaptchaOnFocus, false);
+document.getElementById('NACHNAME').addEventListener('focus', reCaptchaOnFocus, false);
+document.getElementById('EMAIL').addEventListener('focus', reCaptchaOnFocus, false);
