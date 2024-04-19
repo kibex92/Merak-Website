46edbf04cfb6ab22218d2ae958978bc1221349fa
Boris Knezevic <boris.knezevic0@gmail.com>
2024-04-19 16:12:07 +0200

    Reverse the concerts.json to get rid of the sorting function

--- javascript/concerts.js
+++ javascript/concerts.js
@@ -1,7 +1,5 @@
 import { API } from "./api.js";
-import { sortConcerts } from "./calendar";
 
-export async function fetchConcerts() {
-  const concerts = await API.fetchConcerts();
-  return sortConcerts(concerts);
+export default async function fetchConcerts() {
+  return await API.fetchConcerts();
 }
--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,9 @@
-import { fetchConcerts } from "./concerts.js";
+import fetchConcerts from "./concerts.js";
 import { addConcertSectionsToEventContainers, groupConcertsByMonthAndYear, eventContainers } from "./calendar.js";
-let concerts = await fetchConcerts();
 
-addConcertSectionsToEventContainers(eventContainers, groupConcertsByMonthAndYear(concerts));
+async function addConcerts() {
+    let concerts = await fetchConcerts();
+    addConcertSectionsToEventContainers(eventContainers, groupConcertsByMonthAndYear(concerts));
+}
+
+window.addEventListener("load", addConcerts);
