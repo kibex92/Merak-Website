b8636dd6be35426a5007368e6f2c9a48bca4d9e4
Boris Knezevic <boris.knezevic0@gmail.com>
2024-04-23 15:38:19 +0200

    add code from menu.js to utility.js

--- javascript/utility.js
+++ javascript/utility.js
@@ -118,4 +118,15 @@ function handleScroll() {
   }
 }
 
+// Add "scrolled" class to header on scroll
+const header = document.querySelector(".main-header");
+window.addEventListener("scroll", () => {
+	const scrollPosition = window.scrollY;
+	if (scrollPosition > 10) {
+		header.classList.add("scrolled");
+	} else {
+		header.classList.remove("scrolled");
+	}
+});
+
 window.addEventListener("scroll", handleScroll);
